1483ef9e6658eaec1a4fc465eb08d65e03f31c4e
Michael Collins <michael.collins@naked.software>
2025-04-27 04:26:09 +0000

    build(web): update web Dockerfile
    I updated the Dockerfile for the web application to host and expost the
    HTTP server on port 80.
    
    I fixed the configuration for connecting to PostgreSQL to use SSL for
    the production environment.

--- src/web/config/runtime.exs
+++ src/web/config/runtime.exs
@@ -143,13 +143,13 @@ if config_env() == :prod do
   config :time, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :time, NakedTimeWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 80, scheme: "http"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      ip: :any,
       port: port
     ],
     secret_key_base: secret_key_base
